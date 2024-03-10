a779b299094b8661dd0c8c2b6dc07beda18e380d
mahiuddinTuhin <mahiuddiinntuhin@gmail.com>
2024-03-10 20:16:30 +0600

    refact server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import app from "./app";
+
 /* config env */
 if (process.env.NODE_ENV !== "PRODUCTION") {
   dotenv.config({
@@ -11,7 +13,24 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
 
-/*  uncaught exception handling  */
+const uri = process.env.DB_URI as string;
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
+
+/* uncaught exception handling */
 process.on("uncaughtException", (err) => {
   console.log("Error:" + err?.message);
   console.log("Stack:" + err?.stack);
@@ -22,7 +41,6 @@ process.on("uncaughtException", (err) => {
 /* unhandled promise rejection */
 process.on("unhandledRejection", (err: any) => {
   console.log("Shutting down the server for: " + err?.message);
-
   console.log("Shutting down the server for uncaught exception!");
 
   server.close(() => {
